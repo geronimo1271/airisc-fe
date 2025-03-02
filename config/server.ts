2d7601aa50d551b2fa4cd65b63d46265415c360a
Giorgio Dal Maso <giorgio.dalmaso@develon.com>
2025-03-02 17:57:29 +0100

    fix

--- config/server.ts
+++ config/server.ts
@@ -2,15 +2,15 @@ import { env } from '@strapi/utils';
 import cronTasks from './cron-tasks';
 
 export default () => ({
-  host: env('SERVER_HOST', '127.0.0.1'),
-  port: env.int('SERVER_PORT', 1337),
+  host: env('HOST', '127.0.0.1'),
+  port: env.int('PORT', 3000),
   app: {
     keys: env.array('APP_KEYS'),
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
-  url: env('SERVER_HOSTNAME', 'http://localhost:1337'),
+  url: env('HOSTNAME', 'http://localhost:3000'),
   cron: {
     enabled: true,
     tasks: cronTasks,
